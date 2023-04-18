e4189913bb60ecb2c937008f6995aa4d060a6512
YewqLwp <106021192+YewqLwp@users.noreply.github.com>
2023-04-18 15:25:09 +0800

    coomit

---
+++ www.likwidasset.com/Scripts/ListingPage.js
@@ -0,0 +1,216 @@
+﻿/*
+
+Author  : Se Chien Yiing 
+Description: This file contains functions that are used on listing page
+*/
+
+var map;
+var bounds;
+var markers = [];
+var myLatLng;
+var currentClick;
+var nearbyDetail;
+var centerLat = parseFloat($('#googleMap').data("lat"));
+var centerLng = parseFloat($('#googleMap').data("lng"));
+var directionsService;
+var directionsDisplay;
+
+//Initialize Google Map
+function initMap() {
+    myLatLng = { lat: centerLat, lng: centerLng };
+    bounds = new google.maps.LatLngBounds();
+
+    if (country != 'CN')
+    {
+        directionsService = new google.maps.DirectionsService;
+        directionsDisplay = new google.maps.DirectionsRenderer;
+    }
+    
+    //initialize map
+    var mapProp = {
+        center: myLatLng,
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.TOP_RIGHT
+        },
+        mapTypeControl: false,
+        scrollwheel: true,
+        scaleControl: true,
+        gestureHandling: "cooperative"
+    };
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
+    
+    //property marker
+    var image = {
+        url: '/content/images/locationmarker4.png',
+        scaledSize: new google.maps.Size(40, 50),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(25, 50)
+    };
+    var propertyName = $('#googleMap').data("property-name") + ' - ' + $('#googleMap').data("property-address");
+    var propertyContent = '<div id="content">' + propertyName + '</div>';
+
+    infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        icon: image,
+        map: map,
+        title: propertyName
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(propertyContent);
+        infowindow.open(map, this);
+    });
+    google.maps.event.addListener(marker, 'mouseover', function () {
+        infowindow.setContent(propertyContent);
+        infowindow.open(map, this);
+    });
+}
+
+function callback(results, status) {    
+    bounds = new google.maps.LatLngBounds();
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+
+        //start to build marker
+        nearbyDetail = '<div class="pure-g nearby-detail">';
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i], i);
+        }
+        nearbyDetail += '</div>';
+
+        currentClick.after(nearbyDetail);
+        myFitBounds(map, bounds);
+        showMarkers();
+    }
+}
+
+//Create Markers
+function createMarker(place, i) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        position: place.geometry.location
+    });
+
+    bounds.extend(marker.position);
+    markers.push(marker);
+
+    nearbyDetail += '<a class="nearby-place pure-u-1" data-nearby-place-index="' + i + '">';
+    nearbyDetail += '<div class="pure-g">';
+    nearbyDetail += '<p class="small pure-u-1 nearby-place-name ut-hide-text-overflow">' + place.name + '</p>';
+    nearbyDetail += '</div>';
+    nearbyDetail += '</a>';
+
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+    google.maps.event.addListener(marker, 'mouseover', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setMapOnAll(null);
+    markers = [];
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
+
+$(document).ready(function () {
+    $('#nearby').on('click', '.nearby-overview', function (e) {
+        var type = $(this).data("nearby-category");
+        var radius = $(this).data("radius");
+        var active = $(this).data("active");
+        var service;
+
+        currentClick = $(this);
+        clearMarkers();
+
+        //reset all checkboxes
+        $('#nearby').find(".fa-square-o").removeClass("hidden");
+        $('#nearby').find(".fa-check-square").addClass("hidden");
+        $('.nearby-detail').remove();
+
+        if (active == false) {
+            service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: myLatLng,
+                radius: radius,
+                type: [type]
+            }, callback);
+            $(".nearby-overview").data("active", false);
+            $(this).data("active", true);
+            $(this).find(".fa-square-o").addClass("hidden");
+            $(this).find(".fa-check-square").removeClass("hidden");
+        }
+        else {
+            $(this).data("active", false);
+            $(this).find(".fa-square-o").removeClass("hidden");
+            $(this).find(".fa-check-square").addClass("hidden");            
+        }
+    });
+
+    $('#nearby').on('mouseover', '.nearby-place', function (e) {
+        var index = $(this).data("nearby-place-index");
+        google.maps.event.trigger(markers[index], 'click');
+    });
+
+    //Travel Time Textbox Auto Complete for typeahead
+    $('#travelTime-box').on("typeahead:autocomplete typeahead:select", function (e, sData) {
+        //Initialize Typeahead function
+        $('#travelTime-box').typeahead('val', sData.terms[0].value);
+
+        //Get Google Map DirectionService
+        directionsService.route({
+            origin: myLatLng,
+            destination: { 'placeId': sData.place_id },
+            travelMode: google.maps.TravelMode.DRIVING
+        }, function (response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {                
+                $('#mapLocation').text(sData.terms[0].value)
+                $('#mapDuration').text(response.routes[0].legs[0].duration.text)
+                $('#mapDistance').text(response.routes[0].legs[0].distance.text)
+
+                directionsDisplay.setMap(null);
+                directionsDisplay.setMap(map);
+                directionsDisplay.setDirections(response);
+                $('#mapDirection').removeClass('hidden');
+            }
+        });
+    });
+
+    $('#travelTimeSearch').on('keydown', '.twitter-typeahead #travelTime-box', function (e) {
+        if (e.which == 13) {
+            e.preventDefault();
+            $('.tt-selectable').first().click();
+        }
+    });
+
+
+
+});
+
---
+++ www.likwidasset.com/Scripts/searchbox.js
@@ -0,0 +1,203 @@
+﻿/*
+Author  : Se Chien Yiing 
+Description: This file contains functions that are used on search box with typeahead.js
+*/
+
+
+$(document).ready(function () {
+    $('#search-box').on("typeahead:autocomplete typeahead:select", function (e, sData) {
+        $('#search-box').typeahead('val', sData.terms[0].value);
+        //alert(JSON.stringify(sData))
+        
+        //Get place ID
+        $('#placeID').val(sData.place_id);
+        $('#viewport').val('');
+        $('#zoom').val('16');
+
+        var request = {
+            placeId: sData.place_id
+        }
+
+        //Send Request to Google PlacesService to get details of the Place_ID
+        var service = new google.maps.places.PlacesService(document.createElement('div'));
+        service.getDetails(request, function (place, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {                
+                $('#lat').val(parseFloat(place.geometry.location.lat()));
+                $('#lng').val(parseFloat(place.geometry.location.lng()));                                
+
+                //Construct Viewport with 4 points
+                if (place.geometry.viewport != null)
+                    $('#viewport').val(place.geometry.viewport.getNorthEast().lat() + ',' + place.geometry.viewport.getNorthEast().lng() + ',' + place.geometry.viewport.getSouthWest().lat() + ',' + place.geometry.viewport.getSouthWest().lng());
+
+                //Submit Search Form
+                $('#searchForm').submit();
+            }
+        });
+    });
+
+    //on Key down handle: Click
+    $('#searchForm').on('keydown', '.twitter-typeahead #search-box', function (e) {
+        if (e.which == 13) {
+            e.preventDefault();
+            $('.tt-selectable').first().click();
+        }
+    });
+
+    //on button click handler
+    $('#submit-search').on('click', function (e) {                        
+        if ($('.tt-selectable').length > 0){
+            e.preventDefault();
+            $('.tt-selectable').first().click();
+        }           
+    });
+
+    
+    //initialize search box with typeahead scripts - restrict to Malaysia only
+    $('#search-box, #location-box, #travelTime-box, #small-search-box #rental-search-box').typeahead(null, {
+        name: 'search-box',
+        limit: Infinity,
+        display: 'description',
+        /*source: function (query, syncResults, asyncResults) {
+            var service = new google.maps.places.AutocompleteService();
+            var place = service.getQueryPredictions({ input: 'klcc' },displaySuggestions );*/
+        source: function (query, syncResults, asyncResults) {
+            var service = new google.maps.places.AutocompleteService();
+            var request = {
+                input: query,
+                componentRestrictions: { country: 'my' },
+            };
+            //Call Google PlacePrediction Services
+            var place = service.getPlacePredictions(request, function (predictions, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    asyncResults(predictions)
+                    return;
+                }                
+            });
+        },
+        templates: {
+            empty: [
+              '<div class="empty-message">',
+                'No result found.',
+              '</div>'
+            ].join('\n'),
+            suggestion:
+                function (data) {
+                    //Construct Auto Suggestion drop dodwn list strings                    
+                    var suggestionText = data.terms[0].value;
+                    suggestionText = suggestionText.substring(0, data.matched_substrings[0].offset) + '<strong>' + suggestionText.substring(data.matched_substrings[0].offset, data.matched_substrings[0].offset + data.matched_substrings[0].length) + '</strong>' + suggestionText.substring(data.matched_substrings[0].offset + data.matched_substrings[0].length)
+                    
+                    var addressText = data.description.substring(data.terms[1].offset);
+                    if (addressText.substring(addressText.length - 9).toUpperCase() == ' MALAYSIA') {
+                        addressText = addressText.substring(0, addressText.length - 10);
+                        addressText = addressText.replace('Federal Territory of Kuala Lumpur', '');
+                        addressText = addressText.replace('Kuala Lumpur,', 'Kuala Lumpur');
+                    }
+                    //Add Icon according to type of location
+                    var iconText = '<td rowspan=\'2\'><span class="fa ';
+                    if (data.types[0] == 'transit_station')
+                        iconText += 'fa-subway';
+                    else
+                        iconText += 'fa-map-marker';
+
+                    iconText += ' typeaheadicon"></span></td>';
+                    return '<table><tr>' + iconText + '<td>' + suggestionText + '</td></tr><tr><td><small class=\'smalltext\'>' + $('<div/>').text(addressText).html() + '</small></td></tr></table>';
+                }
+        }
+    });
+
+
+    //initialize International search box - No Location Restriction
+    $('#international-location-box').typeahead(null, {
+        name: 'international-search-box',
+        limit: Infinity,
+        display: 'description',
+        source: function (query, syncResults, asyncResults) {
+            var service = new google.maps.places.AutocompleteService();
+            var request = {
+                input: query,
+                componentRestrictions: { },
+            };
+            //Call Google PlacePrediction Services
+            var place = service.getPlacePredictions(request, function (predictions, status) {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    asyncResults(predictions)
+                    return;
+                }
+            });
+        },
+        templates: {
+            empty: [
+              '<div class="empty-message">',
+                'No result found.',
+              '</div>'
+            ].join('\n'),
+            suggestion:
+                function (data) {
+                    //Construct Auto Suggestion drop dodwn list strings
+                    var suggestionText = data.terms[0].value;
+                    suggestionText = suggestionText.substring(0, data.matched_substrings[0].offset) + '<strong>' + suggestionText.substring(data.matched_substrings[0].offset, data.matched_substrings[0].offset + data.matched_substrings[0].length) + '</strong>' + suggestionText.substring(data.matched_substrings[0].offset + data.matched_substrings[0].length)
+                   // console.log(data);
+                    if (data.terms.length > 2) {
+                        var addressText = data.description.substring(data.terms[1].offset);
+                    }
+                    //Add Icon according to type of location
+                    var iconText = '<td rowspan=\'2\'><span class="fa ';
+                    if (data.types[0] == 'transit_station')
+                        iconText += 'fa-subway';
+                    else
+                        iconText += 'fa-map-marker';
+
+                    iconText += ' typeaheadicon"></span></td>';
+                    return '<table><tr>' + iconText + '<td>' + suggestionText + '</td></tr><tr><td><small class=\'smalltext\'>' + $('<div/>').text(addressText).html() + '</small></td></tr></table>';
+                }
+        }
+    });
+
+});
+
+//Zoom in to fit the bounds nicely
+function myFitBounds(myMap, bounds) {    
+    myMap.fitBounds(bounds); // calling fitBounds() here to center the map for the bounds
+    
+    var overlayHelper = new google.maps.OverlayView();
+    overlayHelper.draw = function () {
+        if (!this.ready) {
+            var extraZoom = getExtraZoom(this.getProjection(), bounds, myMap.getBounds());
+            if (extraZoom > 0) {
+                myMap.setZoom(myMap.getZoom() + extraZoom);
+            }
+            this.ready = true;
+            google.maps.event.trigger(this, 'ready');
+        }
+    };
+    overlayHelper.setMap(myMap);
+}
+
+//Get extra zoom in so that it wont have too much unrelated spaces at the borders
+function getExtraZoom(projection, expectedBounds, actualBounds) {
+
+    // in: LatLngBounds bounds -> out: height and width as a Point
+    function getSizeInPixels(bounds) {
+        var sw = projection.fromLatLngToContainerPixel(bounds.getSouthWest());
+        var ne = projection.fromLatLngToContainerPixel(bounds.getNorthEast());
+        return new google.maps.Point(Math.round(10000 * Math.abs(sw.y - ne.y)) / 10000, Math.round(10000 * Math.abs(sw.x - ne.x)) / 10000)
+    }
+
+    var expectedSize = getSizeInPixels(expectedBounds),
+        actualSize = getSizeInPixels(actualBounds);
+
+    if (Math.floor(expectedSize.x) == 0 || Math.floor(expectedSize.y) == 0) {
+        return 0;
+    }
+
+    var qx = actualSize.x / expectedSize.x;
+    var qy = actualSize.y / expectedSize.y;
+    var min = Math.min(qx, qy);
+
+    if (min < 1) {
+        return 0;
+    }
+
+    return Math.floor(Math.log(min) / Math.LN2 /* = log2(min) */);
+}
+
